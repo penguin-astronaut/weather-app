d04584e69037eec182fea7bfb4226596d5abef52
Vadim Kasatkin <penguin2804@gmail.com>
2021-05-19 19:49:00 +0400

    html webpack config set settings

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,18 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
+    clean: true,
   },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "./public/index.html",
+      title: "Weather App",
+    }),
+  ],
 };
