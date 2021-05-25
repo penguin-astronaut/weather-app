374232a5f6745023618de36ab25ece80f1cce03e
Vadim Kasatkin <penguin2804@gmail.com>
2021-05-25 12:18:12 +0400

    fix http to https in weather api response

--- src/services.js
+++ src/services.js
@@ -3,7 +3,7 @@ async function getWeatherByCity(city) {
     throw new Error("City can't be empty");
   }
   const key = "619769815d2e235e82cf7ee1a4435658";
-  const baseUrl = "http://api.openweathermap.org/data/2.5/";
+  const baseUrl = "https://api.openweathermap.org/data/2.5/";
 
   const response = await fetch(
     `${baseUrl}weather?q=${city}&appid=${key}&units=metric`
